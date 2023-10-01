3d5f374e8379b1eb0f2792f455b506b2e9acc279
Maxim <toper.one111@gmail.com>
2023-10-01 15:15:29 +0300

    Create a reader class that will read a file number by number

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import generateFile from './fileGeneration';
 import { Command } from 'commander';
 import { validateFilePath } from './fileValidation';
 import NaturalMergeSort from './naturalMerge';
+import readInChunks from './readInChunks';
 
 const program = new Command();
 
@@ -19,9 +20,13 @@ const { file: filePath } = program.opts();
 (async () => {
   await validateFilePath(program, filePath);
 
-  // await naturalMerge(filePath);
-  const sorter = new NaturalMergeSort(filePath);
-  await sorter.sort();
+  // const sorter = new NaturalMergeSort(filePath);
+  // await sorter.sort();
+  const start = performance.now();
+  await readInChunks(filePath);
+  const end = performance.now();
+  const elapsed = end - start;
+  console.log(`Elapsed time: ${elapsed}`);
 })();
 
 // generateFile(1024 * 1024 * 500);
