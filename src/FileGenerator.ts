b1f6707ff3158b6eec6915e724bb06c9f3f1ad06
Maxim <toper.one111@gmail.com>
2023-10-03 13:24:25 +0300

    Improve CLI interface

--- src/FileGenerator.ts
+++ src/FileGenerator.ts
@@ -3,7 +3,16 @@ import Writer from './Writer';
 
 export default class FileGenerator {
   private readonly SEPARATOR: string = ' ';
-  private readonly NUMBERS_IN_LINE: number = 10;
+  private readonly NUMBERS_IN_LINE: number;
+
+  private readonly MIN_NUMBER: number;
+  private readonly MAX_NUMBER: number;
+
+  constructor(N_IN_L: number, MIN: number, MAX: number) {
+    this.NUMBERS_IN_LINE = +N_IN_L;
+    this.MIN_NUMBER = +MIN;
+    this.MAX_NUMBER = +MAX;
+  }
 
   public async generateFile(byteSize: number = 1024 * 1024) {
     const GENERATED_FILENAME = `generated_file_${this.formatBytes(
@@ -39,8 +48,11 @@ export default class FileGenerator {
     await writer.end();
   }
 
-  private getRandInt(min: number = -1000, max: number = 1000) {
-    return min + Math.floor(Math.random() * (max - min)) + 1;
+  private getRandInt() {
+    return (
+      Math.floor(Math.random() * (this.MAX_NUMBER - this.MIN_NUMBER + 1)) +
+      this.MIN_NUMBER
+    );
   }
 
   private formatBytes(bytes: number, decimals = 2) {
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { Command } from 'commander';
-import { validateFilePath } from './fileValidation';
+import { validateFilePath } from './utils/fileValidation';
 import Sorter from './Sorter';
 import { SortingTechnique } from './types';
-import FileGenerator from './fileGeneration';
+import FileGenerator from './FileGenerator';
 
 const program = new Command();
 
@@ -11,26 +11,53 @@ program
   .description('CLI to sort huge files')
   .version('0.1.0');
 
-program.requiredOption('-f, --file <filePath>');
+program
+  .command('sort')
+  .description('Sort a given file externally')
+  .argument('<filePath>', 'path to a file that is need to be sorted')
+  .option(
+    `-t, --type <${Object.values(SortingTechnique).join(', ')}>`,
+    'type of the sorting algorithm',
+    `${SortingTechnique.NATURAL_MERGE}`
+  )
+  .action(async (filePath, { type }) => {
+    await validateFilePath(program, filePath);
+
+    const start = performance.now();
+    const sorter = new Sorter(filePath, type);
+    await sorter.sort();
+    const end = performance.now();
+    console.log(filePath);
+    console.log(`Elapsed: ${(end - start) / 1000}s`);
+  });
+
+program
+  .command('generateFile')
+  .description('Generate a file of dummy numbers')
+  .argument('<fileSize>', 'Size of the file to be generated in bytes')
+  .option(
+    '-min, --minimumNumber <number>',
+    'minimal number that will be generated inside of the file',
+    '-1000'
+  )
+  .option(
+    '-max, --maximumNumber <number>',
+    'maximum number that will be generated inside of the file',
+    '1000'
+  )
+  .option(
+    '-nInL, --numbersInLine <number>',
+    'number of numbers that a single line will contain',
+    '10'
+  )
+  .action(async (fileSize, { minimumNumber, maximumNumber, numbersInLine }) => {
+    console.log(fileSize, minimumNumber, maximumNumber, numbersInLine);
+    const generator = new FileGenerator(
+      numbersInLine,
+      minimumNumber,
+      maximumNumber
+    );
+    await generator.generateFile(fileSize);
+  });
 
 program.parse(process.argv);
-
-const { file: filePath } = program.opts();
-
-(async () => {
-  await validateFilePath(program, filePath);
-
-  const start = performance.now();
-  const sorter = new Sorter(filePath, SortingTechnique.NATURAL_MERGE);
-  await sorter.sort();
-  const end = performance.now();
-  console.log(filePath);
-  console.log(`Elapsed: ${(end - start) / 1000}s`);
-})();
-
-// const generator = new FileGenerator();
-// (async () => {
-//   for (let i = 1024 * 1024 * 20; i < 1024 * 1024 * 100; i *= 2) {
-//     await generator.generateFile(i);
-//   }
-// })();
